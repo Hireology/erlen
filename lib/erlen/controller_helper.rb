dee625b745ef52911f690850fbe239d0b359c501
Tim Brenner <timpbrenner@gmail.com>
2016-11-14 14:04:41 -0500

    Add some code back in

--- lib/erlen/controller_helper.rb
+++ lib/erlen/controller_helper.rb
@@ -16,26 +16,46 @@ module Erlen
       #
       # @param action [String] the name of the action
       # @param request [BaseSchema] the schema for request body
-      def request_schema(action, request)
-        __erlen__create_before_action(action, request)
+      # @param response [BaseSchema] the schema for response body
+      def action_schema(action, request: nil, response: nil)
+        __erlen__create_before_action(action, request, response)
+        __erlen__create_after_action(action, response)
         nil
       end
 
-      def __erlen__create_before_action(action, request_schema)
+      def __erlen__create_before_action(action, request_schema, response_schema)
         define_method(:"validate_request_schema_for_#{action}") do
-          begin
-            data = request.request_parameters
-            data = data[:data] if data.is_a?(Hash)
+          # memoize both of them here
+          @request_schema = request_schema
+          @response_schema = response_schema
+          if request_schema
+            begin
+              data = request.request_parameters
+              data = data[:data] if data.is_a?(Hash)
 
-            @request_payload = Erlen::JSONSerializer.from_json(data, request_schema)
-          rescue JSON::ParserError
-            raise InvalidRequestError.new("Could not parse request body")
+              @request_payload = Erlen::JSONSerializer.from_json(data, request_schema)
+            rescue JSON::ParserError
+              raise InvalidRequestError.new("Could not parse request body")
+            end
+            raise ValidationError.from_errors(@request_payload.errors) unless @request_payload.valid?
           end
-
-          return if @request_payload.valid?
-          raise ValidationError.from_errors(@request_payload.errors)
         end
         send(:"before_action", :"validate_request_schema_for_#{action}", only: action)
+      end
+
+      private
+
+      def __erlen__create_after_action(action, schema)
+        define_method(:"validate_response_schema_for_#{action}") do
+          return if @validated
+          begin
+            json = JSON.parse(response.body)
+          rescue JSON::ParserError
+            raise InvalidResponseError.new("Could not parse response body")
+          end
+          @response_payload = schema.new(json)
+          raise ValidationError.from_errors(@response_payload.errors) unless @response_payload.valid?
+        end
       end
     end
 
@@ -45,8 +65,11 @@ module Erlen
       klass.extend(ClassMethods)
     end
 
-    # This contains the current action's request payload
-    attr_reader :request_payload
+    # This contains the current action's request schema
+    attr_reader :request_schema
+
+    # This contains the current action's response schema
+    attr_reader :response_schema
 
     # Allows rendering of a payload. This operates as the original render if
     # options do not include payload.
@@ -65,8 +88,24 @@ module Erlen
         render_schema(payload, options, extra_options=extra_options, &block)
       else
         @validated = false
+        @response_payload = nil
         super
       end
+    end
+
+    # Payload is an instance of BaseSchema class, representing either a
+    # request body or response body, validated against the schema. This
+    # particular method is only used to retrieve the request payload.
+    def request_payload
+      # raise NoPayloadError if @request_payload.nil?
+      @request_schema.import(@request_payload) if @request_payload
+    end
+
+    # Reads the current response payload, an instance of BaseSchema class.
+    # You can set this value using render().
+    def response_payload
+      # raise NoPayloadError if @response_payload.nil?
+      @response_schema.import(@response_payload) if @response_payload
     end
 
     private
@@ -76,6 +115,7 @@ module Erlen
       opts.update({json: Erlen::JSONSerializer.to_json(payload)})
       render(opts, extra_opts, &blk) # NOTE: indirect recursion!
       @validated = true # set this after recursive render()
+      @response_payload = payload
     end
 
   end
--- spec/erlen/controller_helper_spec.rb
+++ spec/erlen/controller_helper_spec.rb
@@ -26,7 +26,8 @@ describe Erlen::ControllerHelper do
   class JobsController < FauxController
     include Erlen::ControllerHelper
 
-    request_schema :create, JobRequestSchema
+    action_schema :create, request: JobRequestSchema, response: JobResponseSchema
+    action_schema :show, response: JobResponseSchema
 
     def create
       job = JobResponseSchema.import(request_payload)
@@ -60,6 +61,21 @@ describe Erlen::ControllerHelper do
       expect(controller.request_payload.valid?).to be_truthy
       expect(controller.request_payload.class).to be(JobRequestSchema)
       controller.create
+      controller.validate_response_schema_for_create
+      expect(controller.response_payload.valid?).to be_truthy
+      expect(controller.response_schema).to be(JobResponseSchema)
+    end
+    it "validates show schema (without a proper payload)" do
+      request = OpenStruct.new
+      request.body = ""
+      controller.request = request
+      controller.validate_request_schema_for_show
+      expect(controller.request_payload).to be_nil
+      controller.show
+      expect(controller.response_payload).to be_nil
+      controller.validate_response_schema_for_create
+      expect(controller.response_payload.valid?).to be_truthy
+      expect(controller.response_schema).to be(JobResponseSchema)
     end
     it "invalidates malformed request body" do
       request = OpenStruct.new
@@ -68,6 +84,14 @@ describe Erlen::ControllerHelper do
       expect do
         controller.validate_request_schema_for_create
       end.to raise_error(Erlen::InvalidRequestError)
+    end
+    it "invalidates malformed response body" do
+      response = OpenStruct.new
+      response.body = "notavalidjson"
+      controller.response = response
+      expect do
+        controller.validate_response_schema_for_create
+      end.to raise_error(Erlen::InvalidResponseError)
     end
     it "invalidates inappropriate request payload" do
       request = OpenStruct.new
@@ -81,6 +105,19 @@ describe Erlen::ControllerHelper do
       controller.request = request
       expect do
         controller.validate_request_schema_for_create
+      end.to raise_error(Erlen::ValidationError)
+    end
+    it "invalidates inappropriate response payload" do
+      response = OpenStruct.new
+      response.body = '{"wrongattribute": "bar"}'
+      controller.response = response
+      expect do
+        controller.validate_response_schema_for_create
+      end.to raise_error(Erlen::NoAttributeError)
+      response.body = '{}'
+      controller.response = response
+      expect do
+        controller.validate_response_schema_for_create
       end.to raise_error(Erlen::ValidationError)
     end
   end
