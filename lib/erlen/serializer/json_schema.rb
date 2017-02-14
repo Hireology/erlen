05640f88de67e7e715313811e2cf94155d495529
Andrew Zures <andrew.zures@gmail.com>
2017-02-13 20:10:47 -0600

    renaming

--- lib/erlen/serializer/json_schema.rb
+++ lib/erlen/serializer/json_schema.rb
@@ -2,23 +2,13 @@ module Erlen; module Serializer
   class JSONSchema
     include Erlen::Schema
 
-    def self.to_json_schema(erlen_schema_class)
-      build_json_schema_object(erlen_schema_class)
+    def self.to_json_schema(erlen_schema)
+      build_json_schema_object(erlen_schema)
     end
 
-    def self.build_base_object(erlen_schema_class)
-      {
-        type: "object",
-        title: erlen_schema_class.name,
-        description: "expected structure for #{erlen_schema_class.name}",
-        properties: {},
-        required: [],
-      }
-    end
-
-    def self.build_json_schema_object(erlen_schema_class)
-      json_schema_object = build_base_object(erlen_schema_class)
-      erlen_schema_class.schema_attributes.each_pair do |_, attribute|
+    def self.build_json_schema_object(erlen_schema)
+      json_schema_object = build_base_object(erlen_schema)
+      erlen_schema.schema_attributes.each_pair do |_, attribute|
         attribute_name = attribute.name.to_sym
         json_schema_object[:properties][attribute_name] = convert_attribute(attribute.type)
         json_schema_object[:required] << attribute_name if attribute.options[:required]
@@ -27,31 +17,41 @@ module Erlen; module Serializer
      json_schema_object
     end
 
-    def self.build_json_schema_array(erlen_container_class)
+    def self.build_base_object(erlen_schema)
+      {
+        type: "object",
+        title: erlen_schema.name,
+        description: "expected structure for #{erlen_schema.name}",
+        properties: {},
+        required: [],
+      }
+    end
+
+    def self.build_json_schema_array(erlen_container)
       { type: 'list',
         items: {
-          type: convert_attribute(erlen_container_class.element_type),
+          type: convert_attribute(erlen_container.element_type),
         }
       }
     end
 
     def self.convert_attribute(attribute)
       if attribute <= Schema::Base
-        convert_container_type(attribute)
+        convert_collection(attribute)
       else
-        convert_primitive_type(attribute)
+        convert_primitive(attribute)
       end
     end
 
-    def self.convert_container_type(container_attribute)
-      if container_attribute.container_class <= ArrayOf
-        build_json_schema_array(container_attribute)
+    def self.convert_collection(collection_attribute)
+      if collection_attribute.container_class <= ArrayOf
+        build_json_schema_array(collection_attribute)
       else
-        build_json_schema_object(container_attribute)
+        build_json_schema_object(collection_attribute)
       end
     end
 
-    def self.convert_primitive_type(primitive_type)
+    def self.convert_primitive(primitive_type)
       case
       when primitive_type <= String
         "string"
